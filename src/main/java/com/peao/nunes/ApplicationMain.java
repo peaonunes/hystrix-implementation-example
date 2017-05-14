e294af5e441f61198c963fdf9daefa711b49859e
Rafael Nunes G. da Silveira <rngs@cin.ufpe.br>
2017-05-13 22:34:05 -0300

    feat(command): first command implementation

--- src/main/java/com/peao/nunes/ApplicationMain.java
+++ src/main/java/com/peao/nunes/ApplicationMain.java
@@ -1,6 +1,10 @@
 package com.peao.nunes;
 
+import com.peao.nunes.hystrix.commands.ResilientPingCommand;
+
 public class ApplicationMain {
+
+    private static final String URL = "http://localhost:4000/";
 
     public static void main(String[] args) throws Exception {
         long endTimeMillis = System.currentTimeMillis() + 1000;
@@ -8,6 +12,9 @@ public class ApplicationMain {
 
         while (true) {
             if (System.currentTimeMillis() > endTimeMillis) {
+                ResilientPingCommand command = new ResilientPingCommand(URL);
+                message = command.execute();
+
                 System.out.println("- " + message);
                 endTimeMillis = System.currentTimeMillis() + 1000;
             }
--- src/main/java/com/peao/nunes/hystrix/commands/ResilientPingCommand.java
+++ src/main/java/com/peao/nunes/hystrix/commands/ResilientPingCommand.java
@@ -1,15 +1,37 @@
 package com.peao.nunes.hystrix.commands;
 
+import com.netflix.hystrix.HystrixCommand;
+import com.netflix.hystrix.HystrixCommandGroupKey;
 import com.peao.nunes.hystrix.workers.HTTPRequesterWorker;
 
-public class ResilientPingCommand {
+import java.util.HashMap;
 
-    private final String name;
-    private final HTTPRequesterWorker requester;
+public class ResilientPingCommand extends HystrixCommand<String> {
 
-    public ResilientPingCommand(String name, String url) {
-        this.name = name;
-        this.requester = new HTTPRequesterWorker(url);
+    private final HTTPRequesterWorker worker;
+
+    public ResilientPingCommand(String url) {
+        super(Setter
+                .withGroupKey(HystrixCommandGroupKey.Factory.asKey("PingGroup")));
+        this.worker = new HTTPRequesterWorker(url);
     }
 
+    @Override
+    protected String run() throws Exception {
+        System.out.println("--- Is Circuit open? " + this.isCircuitBreakerOpen());
+
+        HashMap<String, String> response = this.worker.healthCheck();
+
+        if(!response.get("exception").isEmpty()) {
+            System.out.println("-- Something went wrong.");
+            throw new RuntimeException(response.get("exception"));
+        } else {
+            return response.get("message");
+        }
+    }
+
+    @Override
+    protected String getFallback() {
+        return "Default fallback";
+    }
 }
